b599c82a5e69b5a2d866bbddf2449741907e2035
Adarsh Chavhan <130956407+adarshchavhan21@users.noreply.github.com>
2023-06-28 21:17:27 +0530

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -29,7 +29,7 @@ app.use(express.json({limit: '10MB'}));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(cors({
-    origin: CLIENT_URL,
+    origin: 'https://ig-social.onrender.com/api',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
